df53c17ec56f55861543d69434321f4b53df4ad4
Gilberto Resendiz <gresendi@uci.edu>
2021-09-30 11:21:20 -0700

    fixed typeError issue

--- index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-router.use('/api', require('./itemRoutes.js'))
-router.use('/api', require('./userRoutes.js'))
-
-module.exports = router
-
-// 6154ca622fb8ed3ff0c94ce6
